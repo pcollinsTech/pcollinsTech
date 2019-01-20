c254471ceb0d2d3b32695eeba4ecb88533916b24
Phil <phil.collins089@gmail.com>
2019-01-20 23:28:03 +0000

    Remove ALgolia

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const config = require("./content/meta/config");
-const transformer = require("./src/utils/algolia");
 
 const query = `{
   allMarkdownRemark( filter: { fields: { slug: { ne: null } } }) {
@@ -21,14 +20,6 @@ const query = `{
   }
 }`;
 
-const queries = [
-  {
-    query,
-    transformer: ({ data }) => {
-      return data.allMarkdownRemark.edges.reduce(transformer, []);
-    }
-  }
-];
 
 module.exports = {
   // pathPrefix: config.pathPrefix,
@@ -54,16 +45,6 @@ module.exports = {
       resolve: `gatsby-plugin-layout`,
       options: {
         component: require.resolve(`./src/layouts/`)
-      }
-    },
-    {
-      resolve: `gatsby-plugin-algolia`,
-      options: {
-        appId: process.env.ALGOLIA_APP_ID ? process.env.ALGOLIA_APP_ID : "",
-        apiKey: process.env.ALGOLIA_ADMIN_API_KEY ? process.env.ALGOLIA_ADMIN_API_KEY : "",
-        indexName: process.env.ALGOLIA_INDEX_NAME ? process.env.ALGOLIA_INDEX_NAME : "",
-        queries,
-        chunkSize: 10000 // default: 1000
       }
     },
     {
