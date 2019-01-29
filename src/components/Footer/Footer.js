c9d545f52a73a960785433d6e082e8502eb41116
pcollinsTech <phil@pcollins.tech>
2019-01-29 12:45:04 +0000

    mobile styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,24 +5,58 @@ import { FaFacebook, FaInstagram, FaTwitter, FaEnvelope, FaPhone } from "react-i
 const Footer = props => {
   const { html, theme } = props;
 
-  return <React.Fragment>
+  return (
+    <React.Fragment>
       <div className="main">
         <div className="container">
           <div className="item">
             <h2>Contact</h2>
-            <br/>
-            <p><a href="mailto:pcollins.tech" taarget="__blank" ><span className="icon"><FaEnvelope /></span>pcollins.tech</a></p>
-            <p><a href="tel:07944305605" taarget="__blank" ><span className="icon"><FaPhone /></span>07944305605</a></p>
+            <br />
+            <p>
+              <a href="mailto:pcollins.tech" taarget="__blank">
+                <span className="icon">
+                  <FaEnvelope />
+                </span>
+                pcollins.tech
+              </a>
+            </p>
+            <p>
+              <a href="tel:07944305605" taarget="__blank">
+                <span className="icon">
+                  <FaPhone />
+                </span>
+                07944305605
+              </a>
+            </p>
           </div>
           <div className="item">
             <h2>Social</h2>
-            <br/>
+            <br />
 
-            <a href="https://www.facebook.com/pcollins.tech/"  taarget="__blank" className="social"><span className="iconSocial"><FaFacebook /></span></a>
-            <a href="https://www.instagram.com/pcollins.tech/"  taarget="__blank" className="social"><span className="iconSocial"><FaInstagram /></span></a>
-            <a href="https://www.twitter.com/pcollins.tech/"  taarget="__blank" className="social"><span className="iconSocial"><FaTwitter /></span></a>
+            <a
+              href="https://www.facebook.com/pcollins.tech/"
+              taarget="__blank"
+              className="social"
+            >
+              <span className="iconSocial">
+                <FaFacebook />
+              </span>
+            </a>
+            <a
+              href="https://www.instagram.com/pcollins.tech/"
+              taarget="__blank"
+              className="social"
+            >
+              <span className="iconSocial">
+                <FaInstagram />
+              </span>
+            </a>
+            <a href="https://www.twitter.com/pcollins.tech/" taarget="__blank" className="social">
+              <span className="iconSocial">
+                <FaTwitter />
+              </span>
+            </a>
           </div>
-          
         </div>
         <footer className="footer" dangerouslySetInnerHTML={{ __html: html }} />
       </div>
@@ -33,43 +67,41 @@ const Footer = props => {
           background: ${theme.color.brand.primary};
           padding: ${theme.space.inset.default};
           text-align: center;
-          color:${theme.color.neutral.gray.g};
+          color: ${theme.color.neutral.gray.g};
         }
         h2 {
           font-size: 30px;
         }
-        p{
-        font-size: 20px;
+        p {
+          font-size: 16px;
         }
         a {
           text-decoration: none;
         }
-        .icon{
-          font-size: 20px;
-          padding: 30px 20px 30px 0;
+        .icon {
+          font-size: 18px;
+          padding: 20px 0 20px 0;
         }
         .iconSocial {
-          font-size: 35px;
-           padding: 20px 20px 20px 0;
+          font-size: 20px;
+          padding: 20px 5px 20px 0;
         }
-        a:hover{
-          color:${theme.color.brand.secondary};
+        a:hover {
+          color: ${theme.color.brand.secondary};
         }
         .container {
           margin: 0 auto;
           display: flex;
           justify-content: space-between;
-          padding:${theme.space.inset.default};
-           max-width: ${theme.text.maxWidth.homepage};
-           .item {
-             text-align: left;
-             width:390px;
-             padding:0 20px;
-           }
+          padding-bottom: 2em;
+          max-width: ${theme.text.maxWidth.homepage};
+          .item {
+            text-align: left;
+            width: 390px;
+            padding: 0 20px;
+          }
         }
         .footer {
-          
-
           :global(ul) {
             list-style: none;
             text-align: center;
@@ -94,10 +126,25 @@ const Footer = props => {
           }
         }
 
-        @from-width desktop {.footer {
+        @from-width desktop {
+          .footer {
             padding: 1em 1em 0 1em;
-          }}`}</style>
-    </React.Fragment>;
+          }
+          p {
+            font-size: 20px;
+          }
+          .icon {
+            font-size: 25px;
+            padding: 30px 20px 30px 0;
+          }
+          .iconSocial {
+            font-size: 35px;
+            padding: 20px 20px 20px 0;
+          }
+        }
+      `}</style>
+    </React.Fragment>
+  );
 };
 
 Footer.propTypes = {
