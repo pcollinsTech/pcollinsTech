3fbfa85f324a5f3915f6c07712d5f5137da7b3c8
Phil <phil@pcollins.tech>
2019-09-20 15:03:36 +0100

    About section update for Belfast move

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,13 +2,12 @@ import React from "react";
 import { ThemeContext } from "../layouts";
 import Article from "../components/Article";
 import Headline from "../components/Article/Headline";
-import avatar from "../images/jpg/avatar.jpg"
-import flip from "../images/jpg/flip.jpg"
-import fire from "../images/jpg/fire.jpg"
-import Seo from "../components/Seo"
+import avatar from "../images/jpg/avatar.jpg";
+import flip from "../images/jpg/flip.jpg";
+import fire from "../images/jpg/fire.jpg";
+import Seo from "../components/Seo";
 
 const AboutPage = props => {
-  
   return (
     <React.Fragment>
       <ThemeContext.Consumer>
@@ -25,67 +24,65 @@ const AboutPage = props => {
                   <img src={fire} className="avatar" />
                 </div>
                 <p className="aboutParagraph">
-                  I work mostly in Bournemouth, whilst hopping to and from Belfast, Northern Ireland
-                  (where I'm from). Before I started out building websites, chatbots and apps, I
-                  was working as a chef in the french alps.
+                  I work mostly in Belfast, whilst hopping to and from England. Before I started out
+                  building websites, chatbots and apps, I was working as a chef in the french alps.
                 </p>
                 <p className="aboutParagraph">
-                  Yep, you read that right. A pretty huge career change, but i pulled it off. I
-                  was cooking fine dishes by day, and learning to code and build sites initially
-                  for friends and colleagues by night. Having taken all of those learnings from
-                  the mountains, I packed the snowboard up, swapped my chef knives for coding
-                  tools and headed to Bournemouth where I’ve been now for a number of years.
+                  Yep, you read that right. A pretty huge career change, but i pulled it off. I was
+                  cooking fine dishes by day, and learning to code and build sites initially for
+                  friends and colleagues by night. Having taken all of those learnings from the
+                  mountains, I packed the snowboard up, swapped my chef knives for coding tools and
+                  headed to Bournemouth where I had been living for a number of years. I've now
+                  recently moved back to my home town, Belfast.
                 </p>
                 <p className="aboutParagraph">
                   Since I left working in the mountains, I have collaborated with some of
                   Bournemouth’s most exciting start-ups, agencies and studios. In doing so, I have
-                  been fortunate enough to work next to some of the brightest tech minds the south
-                  has to offer, and i like to think some of their brilliance has rubbed off on me
-                  too.
+                  been fortunate enough to work next to some of the brightest tech minds the South
+                  of England has to offer, and i like to think some of their brilliance has rubbed
+                  off on me too.
                 </p>
 
                 <p className="aboutParagraph">
-                  My long running thought is that I have always wanted to go out on my own and
-                  work with a number of SME’s to help them bring to life their digital projects,
-                  websites and conversational chatbots that help them to stand out amongst the
-                  bigger brands.
+                  My long running ambition is that I have always wanted to go out on my own and work
+                  with a number of SME’s to help them bring to life their digital projects, websites
+                  and conversational chatbots that help them to stand out amongst the bigger brands.
                 </p>
 
                 <p className="aboutParagraph">
                   Creative flair is something I have long since held, having worked as a chef and
-                  then latterly as a developer. So I have got a keen eye for those all important
-                  design touches that can help make a website stand out and engage its user base.
+                  then latterly as a developer. I've got a keen eye for those all important design
+                  touches that can help make a website stand out and engage its user base.
                 </p>
                 <p className="aboutParagraph">
-                  Whilst working as a Chef, I realised I have a strong passion for teaching
-                  people. I always found myself helping out any new members of staff and teaching
-                  them how to do certain dishes/techniques. This give me a huge sense of
+                  Whilst working as a Chef, I realised I have a strong passion for teaching people.
+                  I always found myself helping out any new members of staff and teaching them how
+                  to do certain dishes/techniques. This give me a huge sense of
                   achievement/joy/pleasure.
                 </p>
                 <p className="aboutParagraph">
-                  When I was living in the Alps I had friends and family come out to visit me on
-                  holiday. It was great to see them and spend time with them. I always ended up
-                  being the snowboard / ski instructor for the week. It gave me such joy to share
-                  my knowledge and see their progression.
+                  When I lived in the Alps I had friends and family come out to visit. It was great
+                  to see them and spend time with them. I always ended up teaching them how to
+                  snowboard / ski for the week. It gave me such joy to share my knowledge and see
+                  their progression.
                 </p>
                 <p className="aboutParagraph">
-                  Learning to code has enabled me to combine the 2 passions I have in life,
-                  Learning everyday and sharing my knowledge with others.
+                  Learning to code has enabled me to combine the 2 passions I have in life, Learning
+                  everyday and sharing my knowledge with others.
                 </p>
                 <p className="aboutParagraph">
-                  If you are looking to get your business online or want to find out more on how
-                  you can add functionality to an existing website yourself, get in touch! I'm be
-                  more than happy to help!
+                  If you are looking to get your business online or want to find out more on how you
+                  can add functionality to an existing website yourself, get in touch! I'd be more
+                  than happy to help!
                 </p>
               </div>
             </section>
           </Article>
         )}
       </ThemeContext.Consumer>
-      <Seo  />
+      <Seo />
       <ThemeContext.Consumer>
         {theme => (
-          
           <style jsx="true">{`
             img {
               margin: 0 10px 0px 10px;
@@ -120,6 +117,4 @@ const AboutPage = props => {
   );
 };
 
-
 export default AboutPage;
-
